f236240c80035290d1cef4fd4f646b78bba1f99c
Rob Halff <rob.halff@gmail.com>
2013-07-31 18:59:14 +0200

    different type checking

--- dedot.js
+++ dedot.js
@@ -11,7 +11,7 @@ DeDot._fill = function (a, obj, v, mod) {
     obj[k] = obj[k] || {};
 
     if (obj[k] !== Object(obj[k])) {
-      if(DeDot.override) {
+      if (DeDot.override) {
         obj[k] = {};
       } else {
         throw new Error("Trying to redefine '" + k + "' which is a " + typeof obj[k]);
@@ -32,7 +32,9 @@ DeDot._fill = function (a, obj, v, mod) {
   }
 };
 
-DeDot.process = function(v, mod) {
+DeDot.process = function (v, mod) {
+
+  var i;
 
   if (typeof mod === 'function') {
     v = mod(v);
@@ -50,7 +52,7 @@ DeDot.object = function (obj, mods) {
 
   Object.keys(obj).forEach(function (k, i) {
 
-    var mod = typeof mods !== 'undefined' ? mods[k] : null;
+    var mod = mods === undefined ? null : mods[k];
     if (k.indexOf('.') !== -1) {
       DeDot._fill(k.split('.'), obj, obj[k], mod);
       delete obj[k];
@@ -65,7 +67,7 @@ DeDot.str = function (str, v, obj, mod) {
 
   if (str.indexOf('.') !== -1) {
     DeDot._fill(str.split('.'), obj, v, mod);
-  } else if(DeDot.override) {
+  } else if (DeDot.override) {
     obj[str] = DeDot.process(v, mod);
   }
 
