4437b36aaa86ab92ec7e85abd123fca3d3bb14ee
Rob Halff <rob.halff@gmail.com>
2015-09-30 23:54:49 +0200

    jscs -x

--- test/useArray.js
+++ test/useArray.js
@@ -3,12 +3,12 @@
 require('should');
 var Dot = require('../index');
 
-describe('useArray:', function () {
+describe('useArray:', function() {
 
   var dotObject, arrayObject, object;
   var arrayObjectExpected, objectExpected, dotObjectExpected;
 
-  beforeEach(function () {
+  beforeEach(function() {
 
     dotObject = {
       'a.0': 'value'
@@ -35,36 +35,35 @@ describe('useArray:', function () {
 
   });
 
-
-  it('default is using array ', function () {
+  it('default is using array ', function() {
     var dotLocal = require('../index');
     arrayObjectExpected.should.eql(dotLocal.object(dotObject));
   });
-  it('Should convert dot using arrays ', function () {
+  it('Should convert dot using arrays ', function() {
     Dot.useArray = true;
     arrayObjectExpected.should.eql(Dot.object(dotObject));
   });
-  it('Should convert dot not using arrays ', function () {
+  it('Should convert dot not using arrays ', function() {
     Dot.useArray = false;
     objectExpected.should.eql(Dot.object(dotObject));
   });
-  it('Should convert object using arrays ', function () {
+  it('Should convert object using arrays ', function() {
     Dot.useArray = true;
     arrayObjectExpected.should.eql(Dot.object(dotObject));
   });
-  it('Should convert dot using arrays and convert back equals source', function () {
+  it('Should convert dot using arrays and convert back equals source', function() {
     Dot.useArray = true;
     dotObjectExpected.should.eql(Dot.dot(Dot.object(dotObject)));
   });
-  it('Should convert object using arrays and convert back equals source', function () {
+  it('Should convert object using arrays and convert back equals source', function() {
     Dot.useArray = true;
     arrayObjectExpected.should.eql(Dot.object(Dot.dot(object)));
   });
-  it('Should convert dot not using arrays and convert back equals source', function () {
+  it('Should convert dot not using arrays and convert back equals source', function() {
     Dot.useArray = false;
     dotObjectExpected.should.eql(Dot.dot(Dot.object(dotObject)));
   });
-  it('Should convert object not using arrays and convert back equals source', function () {
+  it('Should convert object not using arrays and convert back equals source', function() {
     Dot.useArray = false;
     objectExpected.should.eql(Dot.object(Dot.dot(arrayObject)));
   });
