732785bafd4b4a2e6b865a5481b73f0a96bae138
Rob Halff <rob.halff@gmail.com>
2016-08-04 12:44:15 +0200

    update dist

--- dist/dot-object.js
+++ dist/dot-object.js
@@ -444,7 +444,7 @@
         tgt = tgt || {}
         path = path || []
         Object.keys(obj).forEach(function(key) {
-            if (Object(obj[key]) === obj[key]) {
+            if (Object(obj[key]) === obj[key] && (Object.prototype.toString.call(obj[key]) === '[object Object]') || Object.prototype.toString.call(obj[key]) === '[object Array]') {
                 return this.dot(obj[key], tgt, path.concat(key))
             } else {
                 tgt[path.concat(key).join(this.seperator)] = obj[key]
