a7e948f2fadf98d8b43f406af1cc456abf8d7fd3
Josh <josh@aluminati.net>
2016-08-04 10:32:56 +0100

    Ensure we only process true Arrays and Objects - This fixes keys with Date values

--- index.js
+++ index.js
@@ -438,7 +438,7 @@ DotObject.prototype.dot = function (obj, tgt, path) {
   tgt = tgt || {}
   path = path || []
   Object.keys(obj).forEach(function (key) {
-    if (Object(obj[key]) === obj[key]) {
+    if (Object(obj[key]) === obj[key] && (Object.prototype.toString.call(obj[key]) === '[object Object]') || Object.prototype.toString.call(obj[key]) === '[object Array]') {
       return this.dot(obj[key], tgt, path.concat(key))
     } else {
       tgt[path.concat(key).join(this.seperator)] = obj[key]
