8a3bf0e692eaf29e22f24faff185eafa8ca746d0
Josh Welham <josh@aluminati.net>
2019-07-17 14:58:36 +0100

    updated to gulp v4

--- gulpfile.js
+++ gulpfile.js
@@ -13,32 +13,35 @@ var DEST = 'dist/'
 
 var paths = ['gulpfile.js', 'src/dot-object.js', 'test/**/*.js']
 
-gulp.task('lint', function () {
+gulp.task('lint', function (done) {
   gulp.src(paths)
     .pipe(standard())
     .pipe(standard.reporter('default', {
       breakOnError: true
     }))
+  done()
 })
 
-gulp.task('mocha', function () {
+gulp.task('mocha', function (done) {
   gulp.src(['test/**/*.js'])
     .pipe(mocha())
     .on('error', gutil.log)
+  done()
 })
 
 gulp.task('watch', function () {
-  gulp.watch(paths, ['build-node', 'mocha'])
+  gulp.watch(paths, gulp.series('build-node', 'mocha'))
 })
 
-gulp.task('build-node', function () {
+gulp.task('build-node', function (done) {
   gulp.src('src/dot-object.js')
     .pipe(hf.footer('\nmodule.exports = DotObject\n'))
     .pipe(rename({ basename: 'index' }))
     .pipe(gulp.dest('./'))
+  done()
 })
 
-gulp.task('build-bower', function () {
+gulp.task('build-bower', function (done) {
   gulp.src('src/dot-object.js')
     .pipe(hf.header('src/header.tpl'))
     .pipe(hf.footer('src/footer.tpl'))
@@ -47,10 +50,11 @@ gulp.task('build-bower', function () {
     .pipe(uglify())
     .pipe(rename({ extname: '.min.js' }))
     .pipe(gulp.dest(DEST))
+  done()
 })
 
-gulp.task('dist', ['lint', 'build-node', 'mocha', 'build-bower'])
+gulp.task('dist', gulp.parallel('lint', 'build-node', 'mocha', 'build-bower'))
 
-gulp.task('test', ['lint', 'build-node', 'mocha'])
+gulp.task('test', gulp.parallel('lint', 'build-node', 'mocha'))
 
-gulp.task('default', ['test'])
+gulp.task('default', gulp.parallel('test'))
